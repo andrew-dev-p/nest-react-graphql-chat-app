1948d8af769e636472b92713b5d2a9a9ff9de2d8
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-05-28 18:10:14 +0300

    Add GraphQL support with user module, including resolvers and services

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
+  app.use(cookieParser());
+
+  const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
+  app.use(graphqlUploadExpress({ maxFileSize: MAX_FILE_SIZE, maxFiles: 1 }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = error.constraints
+            ? Object.values(error.constraints).join(', ')
+            : '';
+          return accumulator;
+        }, {});
+
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
