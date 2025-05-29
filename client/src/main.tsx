f866c72fe8468eadf9d6db8a50f6978fb8807b25
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-05-29 15:42:31 +0300

    Implement Sidebar component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
+// import "@mantine/core/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
