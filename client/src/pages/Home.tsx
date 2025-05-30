c35ad1684e0f940e3fc381a6dc18f613b3b3c715
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-05-30 14:23:35 +0300

    Add chatroom and user management components with GraphQL integration

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,10 @@ import Sidebar from "../components/Sidebar";
 import MainLayout from "../layouts/MainLayout";
 import AuthOverlay from "../components/AuthOverlay";
 import ProfileSettings from "../components/ProfileSettings";
+import RoomList from "../components/RoomList";
+import ProtectedRoutes from "../components/ProtectedRoutes";
+import { Flex } from "@mantine/core";
+import AddChatroom from "../components/AddChatroom";
 
 const Home = () => {
   return (
@@ -10,6 +14,18 @@ const Home = () => {
         <AuthOverlay />
         <ProfileSettings />
         <Sidebar />
+        <ProtectedRoutes>
+          <AddChatroom />
+          <Flex
+            direction={{
+              base: "column",
+              md: "row",
+            }}
+            w="100vw"
+          >
+            <RoomList />
+          </Flex>
+        </ProtectedRoutes>
       </>
     </MainLayout>
   );
